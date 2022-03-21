412899a3e2f3e4ca89e8f037cf0969090b93c47b
Ian Borukhovich <ianboru@gmail.com>
2022-03-20 23:39:37 -0700

    showing table

--- core/views.py
+++ core/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 import pandas as pd
 import os
 from hospital_finder.settings import BASE_DIR
+import json
 data_path = os.path.join(BASE_DIR, './data/unplanned_visits.csv')
 # Create your views here.
 def index(request, path=None):
     unplanned_visits = pd.read_csv(data_path)
     print(unplanned_visits)
-    return render(request, "index.html")
+    json_records = unplanned_visits.reset_index().to_json(orient ='records')
+    data = []
+    data = json.loads(json_records)
+    context = {'d': data}
+    return render(request, "index.html", context)

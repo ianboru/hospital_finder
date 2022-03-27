6e12b29a80ac627943619859159247d1e8e45db2
Ian Borukhovich <ianboru@gmail.com>
2022-03-26 18:27:21 -0700

    added search logic and jquery

--- core/views.py
+++ core/views.py
@@ -4,26 +4,35 @@ import pandas as pd
 import os
 from hospital_finder.settings import BASE_DIR
 import json
+import re
 data_path = os.path.join(BASE_DIR, './data/hvbp_clinical_outcomes.csv')
 # Create your views here.
 def index(request, path=None):
+    print("SETTINGS", BASE_DIR)
     unplanned_visits = pd.read_csv(data_path)
     unplanned_visits.rename(columns={
         "Facility Name" : "hospital",
         "MORT-30-AMI Performance Rate" : "mort_30_ami",
         "MORT-30-COPD Performance Rate" : "mort_30_copd",
     }, inplace=True)
-
+    #sort dataframe based on query param
+    search_string = request.GET.get("search")
+    if search_string:
+        unplanned_visits = unplanned_visits[
+            unplanned_visits["hospital"].str.contains(search_string, regex=True, flags=re.IGNORECASE)
+        ]
+    #sort dataframe based on query param
     sort_string = request.GET.get("sort")
     if sort_string and "-" in sort_string:
         unplanned_visits = unplanned_visits.sort_values(by=[sort_string.replace("-","")], ascending=False)
     elif sort_string and  "-" not in sort_string:
         unplanned_visits = unplanned_visits.sort_values(by=[sort_string], ascending=True)
-       
-    print("SORT STRING",sort_string)
-    
+           
+    #convert dataframe to json
     json_records = unplanned_visits.reset_index().to_json(orient ='records')
     hospital_data = json.loads(json_records)
+
+    #set href sort string for sorting in opposite direction
     if sort_string and "-" in sort_string:
         sort_string = sort_string.replace("-","")
     elif sort_string and  "-" not in sort_string:

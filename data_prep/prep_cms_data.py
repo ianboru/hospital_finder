118309655cc51493620befbbf4d07fadb8484dce
Ian Borukhovich <ianboru@gmail.com>
2024-01-07 13:47:50 -0800

    added hai summary metric pipeline and related data

--- data_prep/prep_cms_data.py
+++ data_prep/prep_cms_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+cwd = os.getcwd()
 
-def load_hcahps_data():
-    cwd = os.getcwd()
+def load_hcahps_data(export_path):
     hcahps_path = os.path.join(cwd, "data/HCAHPS-Hospital.csv")
     hcahps_df = pd.read_csv(hcahps_path)
     hcahps_df = hcahps_df[[
@@ -24,7 +24,30 @@ def load_hcahps_data():
     hcahps_mean_df = hcahps_df[['Facility Name', 'HCAHPS Linear Mean Value']].groupby('Facility Name').mean()
     hcahps_mean_df.rename(columns={"HCAHPS Linear Mean Value":"summary score"}, inplace=True)
     hcahps_mean_df['relative mean'] = hcahps_mean_df["summary score"] - hcahps_mean_df["summary score"].mean()
+   
+    hcahps_export_path = os.path.join(export_path,'hcahps_summary_metrics.csv')
+    hcahps_mean_df.to_csv(hcahps_export_path) 
     # hai_path = os.path.join(BASE_DIR, './data/Healthcare_Associated_Infections-Hospital.csv')
 
-    
-load_hcahps_data()
+def load_hai_data(export_path):
+    hai_path = os.path.join(cwd, "data/Healthcare_Associated_Infections-Hospital.csv")
+    hai_df = pd.read_csv(hai_path)
+    hai_df = hai_df[[
+             'Facility ID', 
+             'Facility Name', 
+             'ZIP Code', 
+             'Measure ID',
+             'Measure Name',
+             'Compared to National'
+             ]]
+    hai_df = hai_df[hai_df['Measure ID'].str.contains('SIR', na = False)]
+    hai_df = hai_df[~hai_df['Compared to National'].str.contains('Not Available', na = False)]
+    hai_df['Compared to National']  = hai_df[['Compared to National']].apply(lambda col:pd.Categorical(col).codes)
+    hai_mean_df = hai_df[['Facility Name', 'Compared to National']].groupby('Facility Name').mean()
+    hai_mean_df['relative mean'] = hai_mean_df["Compared to National"] - hai_mean_df["Compared to National"].mean()
+    hai_export_path = os.path.join(export_path,'hai_summary_metrics.csv')
+    hai_mean_df.to_csv(hai_export_path) 
+
+export_path =  os.path.join(cwd, "data")
+load_hcahps_data(export_path)
+load_hai_data(export_path)

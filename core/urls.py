42c010555b3fe560595d67bfc69a5a6470664f6d
Ian Borukhovich <ianboru@gmail.com>
2024-01-28 13:37:04 -0800

    delete legacy features

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,6 @@ from django.urls import path, include
 from core import views
 urlpatterns = [
     path('', views.index, name="index"),
-    path('graph', views.graph, name="graph"),
-    path('favorite', views.favorite, name="favorite"),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("signup/", views.SignUpView.as_view(), name="signup"),
 
 ]

4135c5a935ba092bd0ced393d8ea81e74bd5e394
Ian Borukhovich <ianboru@gmail.com>
2024-02-03 14:07:54 -0800

    settings static files update

--- hospital_finder/settings.py
+++ hospital_finder/settings.py
@@ -122,8 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "dist-dev")]
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'dist-dev')
+       ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

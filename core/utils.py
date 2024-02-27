3430ee39f9492d38371c37a86dbd8bf078d68640
Ian Borukhovich <ianboru@gmail.com>
2024-02-27 08:19:05 -0800

    removed legacy functions

--- core/utils.py
+++ core/utils.py
@@ -23,25 +23,6 @@ def timeit(f, print_=True):
         return result
     return wrap
 
-### Loading data
-def load_hospital_data():
-    data_path = os.path.join(DATA_DIR, 'hvbp_clinical_outcomes.csv')
-    quality_metrics = pd.read_csv(data_path)
-    quality_metrics.rename(columns={
-        "Facility Name" : "hospital",
-        "MORT-30-AMI Performance Rate" : "mort_30_ami",
-        "MORT-30-COPD Performance Rate" : "mort_30_copd",
-    }, inplace=True)
-    quality_metrics = quality_metrics.drop_duplicates(subset=['hospital'])
-    return quality_metrics
-
-def load_mrsa_data():
-    #need to rename this variable
-    data_path_mrsa_file = os.path.join(DATA_DIR, 'mrsa_bsi_odp_2022.csv')
-    mrsa_metrics = pd.read_csv(data_path_mrsa_file, encoding='latin1')
-    mrsa_metrics = mrsa_metrics.drop_duplicates(subset=['Facility_Name'])
-    return mrsa_metrics
-
 def load_summary_metric(metric_name: str) -> pd.DataFrame:
     """ Loads a DataFrame from a csv file in the data directory"""
     data_path = os.path.join(DATA_DIR, f'{metric_name}_summary_metrics.csv')

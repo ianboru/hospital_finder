ca05cba952faf2733b5b293ba564750f05e8d507
Elijah <elijah.olson123@gmail.com>
2024-06-19 14:50:37 -0700

    updated with loop

--- static/components/ColorLegend.js
+++ static/components/ColorLegend.js
@@ -1,35 +1,32 @@
 import React from 'react';
 
 const colorLegendComponent = () => {
-    // Color legend container style for top-right positioning
     const colorLegendStyle = {
         position: "absolute",
         top: "10px",
         left: "10px",
         zIndex: "1000",
-        background: "rgba(255, 255, 255, 0.9)", // RGBA color with 80% opacity
+        background: "rgba(255, 255, 255, 0.9)",
         borderRadius: "10px",
         padding: "10px",
-        width: "380px",
+        width: "370px",
         height: "20px",
         display: "flex"
     };
 
-          //colorLegend
+    const colorLegendColors = [
+        { color: 'red', label: 'Lower Than Average' },
+        { color: '#e7b416', label: 'Average' },
+        { color: 'green', label: 'Above Average' }
+    ];
+
     const colorLegend = (
-        <div style={colorLegendStyle}>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div style={{ width: '10px', height: '10px', backgroundColor: 'red', marginRight: '5px', fontStyle: 'bold' }}></div>
-            <span>Lower Than Average</span>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center', marginLeft: '15px'}}>
-            <div style={{ width: '10px', height: '10px', backgroundColor: '#e7b416', marginRight: '5px' }}></div>
-            <span>Average</span>
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center', marginLeft: '15px'}}>
-            <div style={{ width: '10px', height: '10px', backgroundColor: 'green', marginRight: '5px' }}></div>
-            <span> Above Average</span>
-          </div>
+        <div style = {colorLegendStyle}>
+            {colorLegendColors.map((item, index) => (
+                <div key = {index} style = {{alignItems: 'center', marginLeft: index === 0 ? '0' : '15px', display: 'flex'}}>
+                    <div style = {{height: '10px', width: '10px', backgroundColor: item.color, marginRight: '5px' }}></div>
+                    <span>{item.label}</span>
+                </div> ))}
         </div>
     );
 

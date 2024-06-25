86864c2970106406269479d89ad9f0782a0a84d9
rrthurein <rrthurein@gmail.com>
2024-06-24 22:33:09 -0700

    add and install decouple

--- hospital_finder/settings.py
+++ hospital_finder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data')

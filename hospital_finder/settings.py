81c5039bae30132b26cec5b3eb6cc52283b67eed
rrthurein <rrthurein@gmail.com>
2024-06-24 22:29:11 -0700

    Add db configuration in settings.py

--- hospital_finder/settings.py
+++ hospital_finder/settings.py
@@ -80,10 +80,21 @@ WSGI_APPLICATION = 'hospital_finder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', cast=int),
+        }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation

046f301e4d28133a4d838c7c82ff232481ada230
rrthurein <rrthurein@gmail.com>
2024-06-30 17:36:58 -0700

    add CAPHS Metric and HAI Metric field in Facility model

--- core/models/facility.py
+++ core/models/facility.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .timestamp import TimeStamp
+from .facility_data import HAIMetrics, CAPHSMetrics
 
 class Facility(TimeStamp):
     facility_name = models.CharField(max_length=100, blank=False) 
@@ -9,6 +10,20 @@ class Facility(TimeStamp):
         'Address',
         on_delete=models.CASCADE,
         related_name='facility',
+        null=True,
+        blank=True,
+    )
+    hai_metric = models.ForeignKey(
+        HAIMetrics,
+        on_delete=models.CASCADE,
+        related_name='HAIMetric',
+        null=True,
+        blank=True,
+    )
+    caphs_metric = models.ForeignKey(
+        CAPHSMetrics,
+        on_delete=models.CASCADE,
+        related_name='CAPHSMetric',
         null=True,
         blank=True,
     )

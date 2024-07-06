2deba928432c9a5722e06d0f8aed41635a15d898
rrthurein <rrthurein@gmail.com>
2024-07-06 16:04:35 -0700

    add facility foreignkey to the hai and caphs model

--- core/models/facility.py
+++ core/models/facility.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from .timestamp import TimeStamp
-from .facility_data import HAIMetrics, CAPHSMetrics
 
 class Facility(TimeStamp):
     facility_name = models.CharField(max_length=100, blank=False) 
@@ -11,20 +10,6 @@ class Facility(TimeStamp):
         'Address',
         on_delete=models.CASCADE,
         related_name='facility',
-        null=True,
-        blank=True,
-    )
-    hai_metric = models.ForeignKey(
-        HAIMetrics,
-        on_delete=models.CASCADE,
-        related_name='HAIMetric',
-        null=True,
-        blank=True,
-    )
-    caphs_metric = models.ForeignKey(
-        CAPHSMetrics,
-        on_delete=models.CASCADE,
-        related_name='CAPHSMetric',
         null=True,
         blank=True,
     )

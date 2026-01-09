967cb1e62afc521083c7d5d45ff2181a1fbac543
Ian Borukhovich <ian@Mac.lan>
2026-01-08 18:04:44 -0800

    banner fix

--- static/components/TitleBanner.js
+++ static/components/TitleBanner.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
- const TitleBanner = props => {
-   return (
-     <div
-       style={{
+const TitleBanner = props => {
+  return (
+    <div
+      style={{
         border: '2px solid black',
         width: '100%',
         backgroundColor: '#0073E6',
@@ -13,34 +13,34 @@ import React from 'react'
         alignItems: 'center',
         color: 'white',
         padding: '5px'
-       }}
-     >
-       <div
-         className='title'
-         style={{
-           fontSize: '2em',
-           fontWeight: 'bold',
-           margin: '0',
-           padding: '0',
-           textAlign: 'center'
-         }}
-       >
-         Welcome to Hospital Finder
-       </div>
+      }}
+    >
+      <div
+        className='title'
+        style={{
+          fontSize: '2em',
+          fontWeight: 'bold',
+          margin: '0',
+          padding: '0',
+          textAlign: 'center'
+        }}
+      >
+        Find Quality Care
+      </div>
 
-       <div
-         className='sub-title'
-         style={{
-           fontSize: '1em',
-           margin: '0',
-           padding: '0',
-           textAlign: 'center'
-         }}
-       >
-         Make good life descisions, don't end up here
-       </div>
-     </div>
-   )
- }
+      <div
+        className='sub-title'
+        style={{
+          fontSize: '1em',
+          margin: '0',
+          padding: '0',
+          textAlign: 'center'
+        }}
+      >
+        Make good life descisions, don't end up here
+      </div>
+    </div>
+  )
+}
 
- export default TitleBanner
+export default TitleBanner
--- static/utils.js
+++ static/utils.js
@@ -4,38 +4,38 @@ const filledStarUnicode = "\u2605"
 const addressToUrl = (address) => {
     const urlAddress = address.replace(/\,/g, '');
     const url = urlAddress.replace(/\ /g, '%20');
-    return  `http://maps.google.com/maps?q=${url}`
+    return `http://maps.google.com/maps?q=${url}`
 }
-  
+
 const getHaiEmoji = (starCount, midPoint) => {
-    if(!starCount){
+    if (!starCount) {
         return ""
     }
-    if (starCount === null||starCount == 'Not Available'||starCount == 'NaN'){
+    if (starCount === null || starCount == 'Not Available' || starCount == 'NaN') {
         return "No Data"
     }
-    
-    switch (true){
+
+    switch (true) {
         case starCount > midPoint:
-            return (<span style={{color:"green"}}>😄 Better Than Average</span>)
+            return (<span style={{ color: "green" }}>😄 Better Than Average</span>)
         case starCount == midPoint:
-            return (<span style={{color:"orange"}}>😐 Average</span>)
+            return (<span style={{ color: "orange" }}>😐 Average</span>)
         case starCount < midPoint:
-            return (<span style={{color:"red"}}>🤢 Worse Than Average</span>)
+            return (<span style={{ color: "red" }}>🤢 Worse Than Average</span>)
     }
 }
-const getHCAHPSStars = (starCount, maxCount=5) => {
-    if (starCount === null||starCount == 'Not Available'||starCount == 'NaN'||starCount == "Not Applicable"){
+const getHCAHPSStars = (starCount, maxCount = 5) => {
+    if (starCount === null || starCount == 'Not Available' || starCount == 'NaN' || starCount == "Not Applicable") {
         return "No Data"
     }
-    if(starCount > 5){
+    if (starCount > 5) {
         //hack if starcount is actuanlly a percentage value
         return starCount + "%"
     }
     return (
         <span>
-            <span style={{color : "gold"}}>{filledStarUnicode.repeat(starCount)}</span>
-            <span style={{color : "lightgray"}}>{filledStarUnicode.repeat(maxCount - starCount)}</span>
+            <span style={{ color: "gold" }}>{filledStarUnicode.repeat(starCount)}</span>
+            <span style={{ color: "lightgray" }}>{filledStarUnicode.repeat(maxCount - starCount)}</span>
         </span>
     )
 }
@@ -43,12 +43,15 @@ const getHCAHPSStars = (starCount, maxCount=5) => {
 const scrollToPlaceResult = (id) => {
     const element = document.getElementById(id)
     if (element) {
-        element.scrollIntoView({behavior: 'smooth'})
+        element.scrollIntoView({ behavior: 'smooth' })
     }
 }
 
 const formatPhoneNumber = (phoneNumber) => {
-    return `(${phoneNumber.substring(0,3)}) ${phoneNumber.substring(3,6)}-${phoneNumber.substring(6,10)} `
+    if (!phoneNumber || phoneNumber.trim() === '') {
+        return '';
+    }
+    return `(${phoneNumber.substring(0, 3)}) ${phoneNumber.substring(3, 6)}-${phoneNumber.substring(6, 10)} `
 }
 
 export {
